30f84fd002c5c401939db69bc935bdca001bb22e
你的用户名 <mangosago.liu@gmail.com>
2025-03-14 10:11:09 +0700

    Fix dependencies

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/pingguo/Desktop/Swap/swapx-doc/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/pingguo/Desktop/Swap/swapx-doc/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/pingguo/Desktop/Swap/swapx-doc/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/pingguo/Desktop/Swap/swapx-doc/src/css/custom.css"),
+];
