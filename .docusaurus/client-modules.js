4d2f9e4a792065861e5d0d6f5b206b883c7092dd
swapx-docs <swapxk@gmail.com>
2025-12-19 14:57:27 +0700

    chore: remove .docusaurus from git tracking

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/Users/mark/VSCODE/docs/node_modules/.pnpm/infima@0.2.0-alpha.45/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/mark/VSCODE/docs/node_modules/.pnpm/@docusaurus+theme-classic@3.6.3_@types+react@19.1.6_acorn@8.14.1_react-dom@18.3.1_react@18.3._deiph3nuw6hl35n5eew6lf77xi/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/mark/VSCODE/docs/node_modules/.pnpm/@docusaurus+theme-classic@3.6.3_@types+react@19.1.6_acorn@8.14.1_react-dom@18.3.1_react@18.3._deiph3nuw6hl35n5eew6lf77xi/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/mark/VSCODE/docs/src/css/custom.css"),
-  require("/Users/mark/VSCODE/docs/node_modules/.pnpm/docusaurus-plugin-image-zoom@3.0.1_@docusaurus+theme-classic@3.6.3_@types+react@19.1.6_acorn@_uddcg2zgox5t3jztl7w7zmqgbm/node_modules/docusaurus-plugin-image-zoom/lib/zoom"),
-];
